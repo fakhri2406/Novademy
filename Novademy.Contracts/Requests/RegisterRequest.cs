644d1c9fa497f77bd740a2ea0020ff7664b9b7c4
fakhri2406 <Gezal_yv57@itstep.edu.az>
2025-03-22 19:30:38 +0400

    fix(auth): make RoleId field required in RegisterRequest

--- Novademy.Contracts/Requests/RegisterRequest.cs
+++ Novademy.Contracts/Requests/RegisterRequest.cs
@@ -8,5 +8,5 @@ public class RegisterRequest
     public required string Email { get; init; }
     public required int PhoneNumber { get; init; }
     public required string Password { get; init; }
-    public int RoleId { get; set; }
+    public required int RoleId { get; set; }
 }
